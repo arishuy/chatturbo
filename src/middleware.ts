867f601f1cdd8db1cfc08e4e96a349d00f6589cb
arishuy <huybui19898@gmail.com>
2023-06-27 13:52:24 +0700

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+	const pathname = req.nextUrl.pathname;
+	const protectedPaths = [
+    "/dashboard",
+	"/message",
+	"/message/[id]",
+	"/people",
+	"/reminder",
+	"/reminder/[id]",
+	"/settings",
+  ];
+	const isPathProtected = protectedPaths?.some((path) => pathname == path || pathname.startsWith(path));
+	const res = NextResponse.next();
+
+	if (isPathProtected) {
+		const token = await getToken({ req });
+		if (!token) {
+			const url = new URL(`/login`, req.url);
+			return NextResponse.redirect(url);
+		}
+	}
+	return res;
+}
