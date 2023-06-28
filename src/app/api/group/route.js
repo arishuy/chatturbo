7bb5f16900889851b8acde8e7552d0739f114509
arishuy <huybui19898@gmail.com>
2023-06-28 16:42:41 +0700

    fix api group

--- src/app/api/group/route.js
+++ src/app/api/group/route.js
@@ -1,6 +1,5 @@
 import connect from "@/utils/db";
 import Group from "@/models/group";
-import User from "@/models/user";
 import Message from "@/models/message";
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
@@ -13,6 +12,6 @@ export const GET = async (req) => {
     return new NextResponse("Unauthenticated", { status: 401 });
   }
   const myId = session.sub;
-  const groups = await Group.find({ members: myId }).populate("members", { name: 1, surname: 1, avatar: 1 }).populate("latestMessage");
+  const groups = await Group.find({ members: myId }).populate("members", { name: 1, surname: 1, avatar: 1 }).populate("latestMessage").sort({ updatedAt: -1 });
   return new NextResponse(JSON.stringify(groups), { status: 200 });
 };
