0afbe058d57af7c238db16bd4b254ba42bbad066
arishuy <huybui19898@gmail.com>
2023-06-28 15:08:35 +0700

    add page

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -1,12 +1,22 @@
+import Image from 'next/image'
 
-import React from 'react'
-const page = () => {
+export default function Home() {
   return (
-    <div>
-      <title>Message</title>
+    <>
+    <title>Chat Turbo 🚀</title>
+    <div className="empty">
+      <h1>
+        <span>C</span>
+        <span>H</span>
+        <span>A</span>
+        <span>T</span>
+        <span>T</span>
+        <span>U</span>
+        <span>R</span>
+        <span>B</span>
+        <span>O</span>
+      </h1>
     </div>
-    
+          </>
   )
 }
-
-export default page
